7a07d8d5d970b0759bbfc96193a29b492d22966d
MayoJarr <jakubmajecki2321@gmail.com>
2023-01-16 20:35:54 +0100

    added reset button

--- script.js
+++ script.js
@@ -8,12 +8,17 @@ function sized() {
 
     inputs.style.cssText = "display: none;";
 
+    /* ------- CREATING CSS GRID -------*/
+
     container.style.cssText =
         "display: grid; grid-template: repeat(" +
         value1 +
         ", minmax(1px, 50px)) / repeat(" +
         value2 +
         ", minmax(1px, 50px));";
+
+    /* ------ CREATING DIVS --------*/
+
     for (let i = 0; i < value1; i++) {
         for (let j = 0; j < value2; j++) {
             const div = document.createElement("div");
@@ -22,20 +27,41 @@ function sized() {
         }
     }
 
-    
-    let divs = document.querySelectorAll('.div');
+    /* ---------- PAINTING DIV ----------- */
+
+    let divs = document.querySelectorAll(".div");
     divs.forEach((div) => {
-        div.addEventListener('mouseover', () => {
+        div.addEventListener("mouseover", () => {
             let color1 = Math.floor(Math.random() * 254);
             let color2 = Math.floor(Math.random() * 254);
             let color3 = Math.floor(Math.random() * 254);
-            div.style.cssText = "background: rgb("+color1+","+color2+","+color3+");";
-        })
+            div.style.cssText =
+                "background: rgb(" +
+                color1 +
+                "," +
+                color2 +
+                "," +
+                color3 +
+                ");";
+        });
     });
+
+    /* -------- RESET BUTTON ------- */
+
+    const reset = document.querySelector(".reset");
+    reset.addEventListener("click", () => {
+        divs.forEach((div) => {
+            div.style.cssText = "background: white;";
+        });
+    });
+
+    // --------- KEY BINDING ---------------
+
+    /*reset.addEventListener('keydown', (event) => {
+        console.log(event.key);
+
+    }); */
 }
 
 const submit = document.querySelector(".submit");
 submit.addEventListener("click", sized);
-
-
-
